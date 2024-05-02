1da7c99d34e4f75f391c19719de76990820aa7df
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-05-02 15:48:43 +0700

    update move

--- game2048/jsGame2048.js
+++ game2048/jsGame2048.js
@@ -9,10 +9,10 @@ window.onload = function () {
 
 function createBoard() {
     board = [
-        [0, 2, 0, 0],
+        [2, 2, 0, 0],
         [2, 2, 2, 2],
         [4, 4, 8, 0],
-        [0, 0, 0, 0]
+        [4, 0, 0, 0]
     ]
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
@@ -31,7 +31,11 @@ function updateTile(tile, num) {
     tile.classList.add('tile')
     if (num > 0) {
         tile.innerText = num
-        tile.classList.add('x' + num.toString())
+        if (num <= 4096) {
+            tile.classList.add('x' + num.toString())
+        } else {
+            tile.classList.add('x8192')
+        }
     }
 }
 
@@ -45,6 +49,12 @@ function updateTile(tile, num) {
 document.addEventListener('keyup', function control(e) {
     if (e.code == 'ArrowLeft') {
         moveLeft()
+    } else if (e.code == 'ArrowRight') {
+        moveRight()
+    } else if (e.code == 'ArrowUp') {
+        moveUp()
+    } else if (e.code = 'ArrowDown') {
+        moveDown()
     }
 })
 
@@ -78,6 +88,57 @@ function moveLeft() {
         board[r] = row
 
         for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + ',' + c.toString())
+            let num = board[r][c]
+            updateTile(tile, num)
+        }
+    }
+}
+
+function moveRight() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r]
+        row.reverse()
+        row = move(row)
+        row.reverse()
+        board[r] = row
+
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + ',' + c.toString())
+            let num = board[r][c]
+            updateTile(tile, num)
+        }
+    }
+}
+// idea: tạo 1 hàng mới là cột của bảng
+// khi trượt cột lên là trượt hàng đó sang trái
+function moveUp() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]]
+        row = move(row)
+        // board[0][c] = row[0]
+        // board[1][c] = row[1]
+        // board[2][c] = row[2]
+        // board[3][c] = row[3]
+
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r]
+            let tile = document.getElementById(r.toString() + ',' + c.toString())
+            let num = board[r][c]
+            updateTile(tile, num)
+        }
+    }
+}
+
+function moveDown() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[2][c], board[3][c]]
+        row.reverse()
+        row = move(row)
+        row.reverse()
+
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r]
             let tile = document.getElementById(r.toString() + ',' + c.toString())
             let num = board[r][c]
             updateTile(tile, num)
